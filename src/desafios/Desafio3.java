c0f746c9c6aa800d1b86210ccf6053676adf1d46
YuriCJardim <yuricjardim@gmail.com>
2023-10-10 11:14:45 -0300

    feat - adicinar função para mostrar os numeros negativos '

--- src/desafios/Desafio3.java
+++ src/desafios/Desafio3.java
@@ -2,19 +2,24 @@ package desafios;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Desafio3 {
     public static void main(String[] args) {
         //Desafio 3 - Verifique se todos os números da lista são positivos:
+        //Desafio 3.1 - Verifique se todos os números da lista são positivos, se se tiver negativo o imprimir:
 
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, -3);
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, -8, 8, 9, 10, 5, 4, -3);
 
         Boolean todosPositivos = numeros.stream().allMatch(numero -> numero > 0);
 
         if (todosPositivos) {
             System.out.println("Todos os numeros são positivos");
         }else {
-            System.out.println("Pelo menos um elemento é negativo");
+            System.out.println("Existem numeros negativo");
+
+            List<Integer> numerosNegativos = numeros.stream().filter(n -> n < 0).collect(Collectors.toList());
+            System.out.println("Tem os seguintes numero (s) " + numerosNegativos);
         }
 
     }
