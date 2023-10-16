3b70e269053b65205864d280542c2859b19d9321
YuriCJardim <yuricjardim@gmail.com>
2023-10-16 11:51:11 -0300

    feat - utilizando a Stream API para remover os valores ímpares da lista e imprima a lista resultante no console, e imprimir a lista com os valores ímpares

---
+++ src/desafios/Desafio04.java
@@ -0,0 +1,20 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Desafio04 {
+    public static void main(String[] args) {
+        //Desafio 4 - Remova todos os valores ímpares:
+        //Desafio 4 - Remova todos os valores pares:
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        List<Integer> numerosPares = numeros.stream().filter(n -> n %2 ==0).collect(Collectors.toList());
+        List<Integer> numerosImapres = numeros.stream().filter(n -> n %2 !=0).collect(Collectors.toList());
+
+        System.out.println(numerosPares);
+        System.out.println(numerosImapres);
+    }
+
+}
